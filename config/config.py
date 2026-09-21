17e60a95a7c963229f0b7e09d0d56e07c11655af
dakotahr <iamdakotahr@gmail.com>
2026-09-21 09:37:22 -0300

    Update botID in config.py

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ prefix = '/'
 room = '69a113aed41925285a28e3cd'
 token = 'c8f061d0ec1d252e8ef407165a9820f64ec11e3106f6095e6910831db935ecfe'
 
-botID = '68654c84f77cce8a0c95eb1b'
+botID = '686523fd88522a5a95892544'
 botName = 'BotiNero'
 ownerName = 'IamDakota'
 
