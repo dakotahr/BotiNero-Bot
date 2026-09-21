2a7689d01d8a79516dfe1cbfbc21d5da1ee72f03
dakotahr <iamdakotahr@gmail.com>
2026-09-21 10:42:49 -0300

    Change room ID in config.py
    Updated room ID to the current World ID.

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 prefix = '/'
 
 # Reemplazamos por tu ID de World actual
-room = '69a113aed41925285a28e3cd'
+room = '686526c6bf34634ef6350a09'
 token = 'c8f061d0ec1d252e8ef407165a9820f64ec11e3106f6095e6910831db935ecfe'
 
 botID = '686523fd88522a5a95892544'
